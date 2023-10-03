0446962c1c57fe2d6850cdc59cf12083f2fcac6a
IMWoo94 <leemeawoo94@gmail.com>
2023-10-03 18:55:07 +0900

    spring db part2 spring boot auto embedded mode apply 2023.10.03

--- itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -1,13 +1,10 @@
 package hello.itemservice;
 
-import javax.sql.DataSource;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import hello.itemservice.config.JdbcTemplateV3Config;
 import hello.itemservice.repository.ItemRepository;
@@ -31,16 +28,16 @@ public class ItemServiceApplication {
 		return new TestDataInit(itemRepository);
 	}
 
-	@Bean
-	@Profile("test")
-	public DataSource dataSource() {
-		log.info("메모리 데이터베이스 초기화");
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("org.h2.Driver");
-		dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
-		dataSource.setUsername("sa");
-		dataSource.setPassword("");
-		return dataSource;
-	}
+	// @Bean
+	// @Profile("test")
+	// public DataSource dataSource() {
+	// 	log.info("메모리 데이터베이스 초기화");
+	// 	DriverManagerDataSource dataSource = new DriverManagerDataSource();
+	// 	dataSource.setDriverClassName("org.h2.Driver");
+	// 	dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+	// 	dataSource.setUsername("sa");
+	// 	dataSource.setPassword("");
+	// 	return dataSource;
+	// }
 
 }

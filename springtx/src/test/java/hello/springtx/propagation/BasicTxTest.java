c840b7e46f7743b070d132295543f7e462db6bb7
IMWoo94 <leemeawoo94@gmail.com>
2023-10-07 15:26:34 +0900

    spring db part2 spring transaction propagation required commit 2023.10.07

--- springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -77,4 +77,22 @@ public class BasicTxTest {
 		txManager.rollback(tx2);
 
 	}
+
+	@Test
+	void inner_commit() {
+		log.info("외부 트랜잭션 시작");
+		TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+		// 신규 트랜잭션인가를 던져주는 메소드
+		log.info("outer.isNewTransaction() = {}", outer.isNewTransaction());
+
+		log.info("내부 트랜잭션 시작");
+		TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+		log.info("inner.isNewTransaction() = {}", inner.isNewTransaction());
+		log.info("내부 트랜잭션 커밋");
+		txManager.commit(inner);
+
+		log.info("외부 트랜잭션 커밋");
+		txManager.commit(outer);
+
+	}
 }

2c22b84024a6e84b837233cef6101b0ad2f1425b
IMWoo94 <leemeawoo94@gmail.com>
2023-10-07 16:53:06 +0900

    spring db part2 spring transaction 각각의 트랜잭션 처리 2023.10.07

--- springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -1,5 +1,6 @@
 package hello.springtx.propagation;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
@@ -21,6 +22,11 @@ class MemberServiceTest {
 	@Autowired
 	LogRepository logRepository;
 
+	/**
+	 * MemberService    @Transactional:OFF
+	 * MemberRepository @Transactional:ON
+	 * LogRepository    @Transactional:ON
+	 */
 	@Test
 	void outerTxOff_success() {
 		// given
@@ -32,6 +38,25 @@ class MemberServiceTest {
 		// then : 모든 데이터가 정상 저장 된다.
 		assertTrue(memberRepository.find(username).isPresent());
 		assertTrue(logRepository.find(username).isPresent());
+	}
+
+	/**
+	 * MemberService    @Transactional:OFF
+	 * MemberRepository @Transactional:ON
+	 * LogRepository    @Transactional:ON Exception
+	 */
+	@Test
+	void outerTxOff_fail() {
+		// given
+		String username = "로그예외_outerTxOff_fail";
+
+		// when
+		assertThatThrownBy(() -> memberService.joinV1(username)).isInstanceOf(RuntimeException.class);
+
+		//then: 완전히 롤백되지 않고, member 데이터가 남아서 저장된다.
+		assertTrue(memberRepository.find(username).isPresent());
+		assertTrue(logRepository.find(username).isEmpty());
+
 	}
 
 }
